195d19cc68783c14b05f4d9437d8d063938240c5
Peter Gombos <pepegombos@gmail.com>
2017-06-06 11:06:45 +0200

    feat: total amount and transaction count now displayed, bug: order bug fix for item price

--- src/components/TransactionHistory/index.js
+++ src/components/TransactionHistory/index.js
@@ -4,6 +4,9 @@ import orderBy from 'lodash/orderBy'
 
 import ListItem from './ListItem'
 
+const toDecimal = (num) =>
+  parseFloat(Math.round(num * 100) / 100).toFixed(2)
+
 class TransactionHistory extends Component {
   constructor (props) {
     super(props)
@@ -28,24 +31,38 @@ class TransactionHistory extends Component {
 
   render () {
     const { transactions } = this.props
-    const orderedTransactions = orderBy(transactions, this.state.order, this.state.sort)
+    const orderedTransactions =
+      orderBy(
+        transactions.map(i => {
+          i.price = parseFloat(i.price)
+          return i
+        }), this.state.order, this.state.sort)
+    const totalAmount = transactions.reduce((mem, item) =>
+      mem + parseFloat(item.price)
+    , 0)
     return (
-      <table>
-        <thead>
-          <tr>
-            <td onClick={() => this.changeOrder('name')}>Name</td>
-            <td onClick={() => this.changeOrder('price')}>Price</td>
-            <td onClick={() => this.changeOrder('createdAt')}>Date</td>
-            <td onClick={() => this.changeOrder('reference')}>Reference</td>
-            <td onClick={() => this.changeOrder('status')}>Status</td>
-          </tr>
-        </thead>
-        <tbody>
-          {orderedTransactions.map(item =>
-            <ListItem key={item.id} item={item} />
-          )}
-        </tbody>
-      </table>
+      <div>
+        <div>
+          <span className='sales'>Sales: {transactions.length}</span>
+          <span className='total'>Total Amount: £{toDecimal(totalAmount)}</span>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <td onClick={() => this.changeOrder('name')}>Name</td>
+              <td onClick={() => this.changeOrder('price')}>Price</td>
+              <td onClick={() => this.changeOrder('createdAt')}>Date</td>
+              <td onClick={() => this.changeOrder('reference')}>Reference</td>
+              <td onClick={() => this.changeOrder('status')}>Status</td>
+            </tr>
+          </thead>
+          <tbody>
+            {orderedTransactions.map(item =>
+              <ListItem key={item.id} item={item} />
+            )}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
