339a615fd12cf843b418b7843dd4bea64a9905c6
Peter Gombos <pepegombos@gmail.com>
2017-06-06 13:29:34 +0200

    feat: NavBar added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MerchantView from '../../containers/MerchantViewContainer'
 
+import NavBar from '../NavBar'
 import './index.css'
 
 const App = () =>
   <div>
+    <NavBar />
     <MerchantView />
   </div>
 
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './NavBar.css'
+
+const Nav = () =>
+  <nav>
+    <span>Tabeo</span>
+  </nav>
+
+export default Nav
